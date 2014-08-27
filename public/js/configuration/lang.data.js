52825e1bba000f2ff4f66da9f23f63a55bd63c3e
proximcreation <louis.perrin@gmail.com>
2014-08-27 16:18:12 +0200

    renommage ajout de fichier de données statiques

---
+++ public/js/configuration/lang.data.js
@@ -0,0 +1,376 @@
+var lang =
+{
+		'front_disconnect':{
+			'FR':'déconnecter',
+			'EN':'disconnect'
+		},
+		'front_profile':{
+			'FR':'profil',
+			'EN':'profile'
+		},
+		'front_register':{
+			'FR':'s’inscrire',
+			'EN':'register'
+		},
+		'front_login':{
+			'FR':'connexion',
+			'EN':'login'
+		},
+		'front_username':{
+			'FR':'utilisateur',
+			'EN':'username'
+		},
+		'front_password':{
+			'FR':'mot de passe',
+			'EN':'password'
+		},
+		'front_structure':{
+			'FR':'structure',
+			'EN':'structure'
+		},
+		'front_localisation':{
+			'FR':'localisation',
+			'EN':'localisation'
+		},
+		'front_school':{
+			'FR':'école',
+			'EN':'school'
+		},
+		'front_promo':{
+			'FR':'promo',
+			'EN':'promo'
+		},
+		'front_ city':{
+			'FR':'ville',
+			'EN':'city'
+		},
+		'front_language':{
+			'FR':'langage',
+			'EN':'language'
+		},
+		'front_save_changes':{
+			'FR':'sauvegarder',
+			'EN':'save'
+		},
+		'front_close':{
+			'FR':'fermer',
+			'EN':'close'
+		},
+		'front_today':{
+			'FR':'aujourd’hui',
+			'EN':'today'
+		},
+		'front_clear':{
+			'FR':'vider',
+			'EN':'clear'
+		},
+		'front_cancel':{
+			'FR':'annuler',
+			'EN':'cancel'
+		},
+		'front_add':{
+			'FR':'ajouter',
+			'EN':'add'
+		},
+		'front_remove':{
+			'FR':'supprimer',
+			'EN':'remove'
+		},
+		'front_profile_manager':{
+			'FR':'Configuration du profil',
+			'EN':'Profile manager'
+		},
+		'front_main_form_title':{
+			'FR':'Saisir une prescription',
+			'EN':'Record a prescription'
+		},
+		'front_treatment':{
+			'FR':'Traitement',
+			'EN':'Treatment'
+		},
+		'front_start_date':{
+			'FR':'date de démarrage',
+			'EN':'start date'
+		},
+		'front_pathology':{
+			'FR':'pathologie',
+			'EN':'pathology'
+		},
+		'front_treat_type':{
+			'FR':'type de traitement',
+			'EN':'treatment type'
+		},
+		'front_treat_init':{
+			'FR':'initialisation',
+			'EN':'initialisation'
+		},
+		'front_follow_sample':{
+			'FR':'suite prélèvement',
+			'EN':'follow sample'
+		},
+		'front_treat_duration':{
+			'FR':'durée (en jours)',
+			'EN':'duration (in days)'
+		},
+		'front_treat_frequency':{
+			'FR':'fréquence (par jours)',
+			'EN':'frequency (per days)'
+		},
+		'front_animals':{
+			'FR':'Animaux',
+			'EN':'Animals'
+		},
+		'front_species':{
+			'FR':'espèce',
+			'EN':'species'
+		},
+		'front_animal_type':{
+			'FR':'type d’animal',
+			'EN':'animal type'
+		},
+		'front_weight':{
+			'FR':'poid (en kg)',
+			'EN':'weight(in kg)'
+		},
+		'front_animal_age':{
+			'FR':'age',
+			'EN':'age'
+		},
+		'front_animal_quantity':{
+			'FR':'nombre',
+			'EN':'quantity'
+		},
+		'front_client_type':{
+			'FR':'type de client',
+			'EN':'client type'
+		},
+		'front_animal_environment':{
+			'FR':'environement',
+			'EN':'environment'
+		},
+		'front_treat_prescription':{
+			'FR':'prescription',
+			'EN':'prescription'
+		},
+		'front_record':{
+			'FR':'enregistrer',
+			'EN':'record'
+		},
+		/* === ERRORS & MESSAGES === */
+		'msg_no_change_saved':{
+			'FR':'Aucun changement à enregistrer',
+			'EN':'No change to save'
+		},
+		'msg_on_logged_out':{
+			'FR':'Vous êtes maintenant déconnecté. À bientôt !',
+			'EN':'You are disconnected. See you later !',
+		},
+	    /* === ENUM TRA_PATHOLOGY ===*/
+		'tra_pathology_respiratory':{
+			'FR': 'respiratoire',
+			'EN': 'respiratory'
+		},
+        'tra_pathology_urinary':{
+        	'FR':'urinaire',
+        	'EN':'urinary'
+        }, 
+        'tra_pathology_genital':{
+        	'FR':'génitale',
+        	'EN':'genital'
+        }, 
+        'tra_pathology_mastitis':{
+        	'FR':'mammite',
+        	'EN':'mastitis'
+        }, 
+        'tra_pathology_digestive':{
+        	'FR':'digestive',
+        	'EN':'digestive'
+        }, 
+        'tra_pathology_peritonitis':{
+        	'FR':'péritonite',
+        	'EN':'peritonitis'
+        }, 
+        'tra_pathology_trauma':{
+        	'FR':'traumatologie',
+        	'EN':'trauma'
+        }, 
+        'tra_pathology_limping':{
+        	'FR':'boiterie',
+        	'EN':'limping'
+        }, 
+        'tra_pathology_post-surgery':{
+        	'FR':'post-chirurgie',
+        	'EN':'post-surgery'
+        }, 
+        'tra_pathology_ocular':{
+        	'FR':'oculaire',
+        	'EN':'ocular'
+        }, 
+        'tra_pathology_fever':{
+        	'FR':'fièvre',
+        	'EN':'fever'
+        }, 
+        'tra_pathology_other':{
+	    	'FR':'autre',
+	    	'EN':'other'
+	    },
+	    /* === ENUM TRA_DISPENSE_MODE === */
+	    'tra_dm_inj':{
+	    	'FR':'injection',
+	    	'EN':'injection'
+	    },
+        'tra_dm_im':{
+	    	'FR':'intra-mammaire',
+	    	'EN':'intra-mammary'
+	    },
+        'tra_dm_iu':{
+	    	'FR':'intra-utérine',
+	    	'EN':'intra-uterine'
+	    },
+        'tra_dm_in':{
+	    	'FR':'inhalation',
+	    	'EN':'inhalation'
+	    },
+        'tra_dm_po':{
+	    	'FR':'per-os',
+	    	'EN':'per-os'
+	    },
+        'tra_dm_loc':{
+	    	'FR':'local',
+	    	'EN':'local'
+	    },
+	    /* === ENUM VET_ACTIVITIES ===*/
+	    'vet_activities_cattle':{
+	    	'FR':'bovin',
+	    	'EN':'cattle'
+	    }, 
+        'vet_activities_porcine':{
+	    	'FR':'porcine',
+	    	'EN':'porcine'
+	    }, 
+        'vet_activities_goat':{
+	    	'FR':'caprin',
+	    	'EN':'goat'
+	    }, 
+        'vet_activities_equine':{
+	    	'FR':'équine',
+	    	'EN':'equine'
+	    }, 
+        'vet_activities_canine':{
+	    	'FR':'canine',
+	    	'EN':'canine'
+	    },
+	    /* === ENUM TRA_TYPE ===*/
+        'tra_type_local':{
+	    	'FR':'local',
+	    	'EN':'local'
+	    }, 
+        'tra_type_general':{
+	    	'FR':'général',
+	    	'EN':'general'
+	    },
+	    /* === ENUM TRA_INIT ===*/
+        'tra_init_initial':{
+	    	'FR':'initial',
+	    	'EN':'initial'
+	    }, 
+        'tra_init_extension':{
+	    	'FR':'extension',
+	    	'EN':'extension'
+	    },
+	    /* === ENUM ANIMALS ===*/
+	    'ani_sp_bovin' : {
+	    	'FR':'bovin',
+	    	'EN':'cattle'
+	    },
+	    'ani_sp_ovin' : {
+	    	'FR':'ovin',
+	    	'EN':'ovine'
+	    },
+	    'ani_sp_caprin' : {
+	    	'FR':'caprin',
+	    	'EN':'goat'
+	    },
+	    'ani_sp_porcin' : {
+	    	'FR':'porcin',
+	    	'EN':'porcine'
+	    },
+    	'ani_sp_canin' : {
+	    	'FR':'canin',
+	    	'EN':'canine'
+	    },
+		'ani_ty_meat' : {
+	    	'FR':'viande',
+	    	'EN':'meat'
+	    },
+	    'ani_ty_milk' : {
+	    	'FR':'lait',
+	    	'EN':'milk'
+	    },
+	    'ani_ty_cat' : {
+	    	'FR':'chat',
+	    	'EN':'cat'
+	    },
+    	'ani_ty_dog' : {
+	    	'FR':'chien',
+	    	'EN':'dog'
+	    },
+		'ani_ag_veau' : {
+	    	'FR':'veau',
+	    	'EN':'veal'
+	    },
+		'ani_ag_veau_butchery' : {
+	    	'FR':'veau de boucherie',
+	    	'EN':'butchery veal'
+	    },
+		'ani_ag_pre_livestock' : {
+	    	'FR':'pre troupeau',
+	    	'EN':'pre livestock'
+	    },
+		'ani_ag_fattening' : {
+	    	'FR':'engraissement',
+	    	'EN':'fattening'
+	    },
+		'ani_ag_adult' : {
+	    	'FR':'adulte',
+	    	'EN':'adult'
+	    },
+		'ani_ag_piglet_with_mother' : {
+	    	'FR':'porcelet sous la mère',
+	    	'EN':'piglet with mother'
+	    },
+		'ani_ag_post_weaning' : {
+	    	'FR':'post sevrage',
+	    	'EN':'post weaning'
+	    },
+		'ani_ag_reproducer' : {
+	    	'FR':'reproducteur',
+	    	'EN':'reproducer'
+	    },
+		
+	    'ani_environment_alone' : {
+	    	'FR':'isolé',
+	    	'EN':'alone'
+	    }, 
+        'ani_environment_group' : {
+	    	'FR':'groupe',
+	    	'EN':'group'
+	    }, 
+        'ani_environment_building' : {
+	    	'FR':'batiment',
+	    	'EN':'building'
+	    }, 
+        'ani_environment_grazing' : {
+	    	'FR':'patûrage',
+	    	'EN':'grazing'
+	    },
+
+	    'ani_client_professional':{
+	    	'FR':'professionel',
+	    	'EN':'professional'
+	    }, 
+        'ani_client_private':{
+	    	'FR':'particulier',
+	    	'EN':'private'
+	    }
+};
