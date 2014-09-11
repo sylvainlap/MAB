64fdc608c4568996b3b02f2bac65ab07404ecc5f
proximcreation <louis.perrin@gmail.com>
2014-09-11 10:15:17 +0200

    Fix : body centré, directive de add button (Issue #12)

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -2,9 +2,12 @@ mabapp.directive('autoheight', function(){
 	return {
 		restrict: 'A',
 		link: function(scope, element, attrs){
-			var elt = $(element[0]);
-			elt.ready(function(){
-				elt.height(elt.parent().height());
+			var elt = element[0];
+			var getParentH = function(e){
+				return e.parentNode.offsetHeight;
+			};
+			scope.$watch(function(){return getParentH(elt);}, function(){
+				elt.style.height = getParentH(elt)+'px';
 			});
 		}
 	};
